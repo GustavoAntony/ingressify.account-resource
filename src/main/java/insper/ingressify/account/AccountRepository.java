2bace6d715221051104616c9301f7bf93116d536
eriksoaress <erikbtu2020@gmail.com>
2024-02-23 07:50:11 -0300

    service, account, model, repository

---
+++ src/main/java/insper/ingressify/account/AccountRepository.java
@@ -0,0 +1,8 @@
+package insper.ingressify.account;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AccountRepository extends CrudRepository<AccountModel, String>{
+    Optional<AccountModel> findByEmailAndHash(String email, String hash);
+}
