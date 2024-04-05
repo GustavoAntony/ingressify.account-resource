46779602b840c97d9862290d6d305b9c67515537
eriksoaress <erikbtu2020@gmail.com>
2024-04-05 16:26:04 -0300

    decorador feign

--- src/main/java/jogayjoga/account/AccountApplication.java
+++ src/main/java/jogayjoga/account/AccountApplication.java
@@ -1,8 +1,12 @@
 package jogayjoga.account;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AccountApplication {
 
--- src/main/java/jogayjoga/account/AccountRepository.java
+++ src/main/java/jogayjoga/account/AccountRepository.java
@@ -2,7 +2,9 @@ package jogayjoga.account;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountRepository extends CrudRepository<AccountModel, String>{
     Optional<AccountModel> findByEmailAndHash(String email, String hash);
 }
